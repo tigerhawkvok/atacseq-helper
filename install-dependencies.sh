dd4323c72e7b260d82ccc4b81e0ac43ea4a57b50
Philip Kahn <tigerhawkvok@gmail.com>
2017-12-13 00:01:17 -0800

    install script

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,1 +1,40 @@
 #!/bin/bash
+
+brew update
+brew install wget
+# Install Python3
+brew install python3
+# Install perl
+brew install perl
+PATH=/usr/local/Cellar/perl/perl5/bin:$PATH
+# Install pip
+wget https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py
+python get-pip.py
+## Install Homer and cpanm
+mkdir ~/homerdl
+cd ~/homerdl
+wget http://homer.ucsd.edu/homer/configureHomer.pl
+# Install cpanm
+~/perl5/bin/cpanm Statisics::R
+# Install homer
+perl configureHomer.pl -install
+# Install Samtools
+brew install autoconf
+brew install automake
+cd ~
+git clone https://github.com/samtools/samtools.git
+cd samtools
+autoreconf
+./configure
+make
+make install
+# Install Bedtools
+brew tap homebrew/science
+brew install bedtools
+# Install Scipy packages
+python3 -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
+python -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
+# Install htseq
+pip3 install htseq
+pip install htseq
