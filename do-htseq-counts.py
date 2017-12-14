68cb65b9e1f8572d2abe79f6474a22e002393c55
Philip Kahn <tigerhawkvok@gmail.com>
2017-12-13 16:29:20 -0800

    make way more elegant

--- do-htseq-counts.py
+++ do-htseq-counts.py
@@ -11,12 +11,13 @@ https://github.com/simon-anders/htseq/blob/41ac2e51f64a1fb38129ceabf0f06a3e0e378
 """
 from HTSeq.scripts import count
 import os
+import glob
+
 
 dirName = "CountingResults"
 if not os.path.exists(dirName):
     os.mkdir(dirName)
 else:
-    import glob
     i = 0
     for oldRead in glob.glob(dirName+"/*counts.txt"):
         os.unlink(oldRead)
@@ -32,47 +33,55 @@ else:
 # -t exon
 # -f bam
 
-fileSets = [
-    # Format:
-    # ["bamfile", "gff3", "label"]
-    ["AT-A-10_S4_L001_BowtieOut.sorted.q2.bam", "M-LYR-IN-11-CONTROLandDROUGHT.gff3", "CON1"],
-    ["AT-B-38_S22_BowtieOut.sorted.q2.bam", "M-LYR-IN-11-CONTROLandDROUGHT.gff3", "CON2"]
-]
+bamPrettyMap = {
+    "CON1": "AT-A-10_S4_L001_BowtieOut.sorted.q2.bam",
+    "CON2": "AT-B-38_S22_BowtieOut.sorted.q2.bam"
+}
 
 
-for i, filePair in enumerate(fileSets):
-    print("Counting file "+str(i+1)+" of "+str(len(fileSets)))
-    ############################################
-    # Default arguments and argument order interpreted from here:
-    # https://github.com/simon-anders/htseq/blob/41ac2e51f64a1fb38129ceabf0f06a3e0e37825e/python3/HTSeq/scripts/count.py#L345-L455
-    #
-    # Sample:
-    #
-    # htseq-count -m union -s no --nonunique all -i gene_id -t exon -f bam AT-A-10_S4_L001_BowtieOut.sorted.q2.bam M-LYR-IN-11-CONTROLandDROUGHT.gff3 > M-LYR-IN-11-CONandDROUGHT-CON1-counts.txt
-    ############################################
-    outputString = count.count_reads_in_features(
-        filePair[0], # BAM file
-        filePair[1], # GFF3 file
-        "bam", # -f flag
-        "name", # default
-        30000000, # default
-        "no", # -s flag
-        "union", # -m flag
-        "all", # --nonunique flag
-        "score", # default
-        "score", # default
-        "exon", # -t flag
-        "gene_id", # -i flag, default
-        (), # Default
-        None, # default
-        10, # default
-        "" # default
-        )
-    ## Issue mentioned by Marko
-    #outputString.replace("\n\t", ", ")
-    gff3 = filePair[1][:-5]
-    outFile = gff3 + "-" + filePair[2] + "-counts.txt"
-    fullPath = dirName+"/"+outFile
-    with open(fullPath, "w") as fileWriter:
-        fileWriter.write(outputString)
-        print("Wrote counts to `"+fullPath+"`")
+i = 0
+# Find all the gff3 files
+gff3Pool = glob.glob("*.gff3")
+# Get the total file count
+filePool = len(gff3Pool) * len(bamPrettyMap)
+for gff3File in gff3Pool:
+    # Map every gff3 onto every bam file
+    for label, bamFile in bamPrettyMap.items():
+        i += 1
+        print("Counting file "+str(i+1)+" of "+str(filePool))
+        ############################################
+        # Default arguments and argument order interpreted from here:
+        # https://github.com/simon-anders/htseq/blob/41ac2e51f64a1fb38129ceabf0f06a3e0e37825e/python3/HTSeq/scripts/count.py#L345-L455
+        #
+        # Sample:
+        #
+        # htseq-count -m union -s no --nonunique all -i gene_id -t exon -f bam AT-A-10_S4_L001_BowtieOut.sorted.q2.bam M-LYR-IN-11-CONTROLandDROUGHT.gff3 > M-LYR-IN-11-CONandDROUGHT-CON1-counts.txt
+        ############################################
+        outputString = count.count_reads_in_features(
+            bamFile, # BAM file
+            gff3File, # GFF3 file
+            "bam", # -f flag
+            "name", # default
+            30000000, # default
+            "no", # -s flag
+            "union", # -m flag
+            "all", # --nonunique flag
+            "score", # default
+            "score", # default
+            "exon", # -t flag
+            "gene_id", # -i flag, default
+            (), # Default
+            None, # default
+            10, # default
+            "" # default
+            )
+        ## Issue mentioned by Marko
+        #outputString.replace("\n\t", ", ")
+        gff3 = gff3File[:-5]
+        outFile = gff3 + "-" + label + "-counts.txt"
+        fullPath = dirName+"/"+outFile
+        with open(fullPath, "w") as fileWriter:
+            fileWriter.write(outputString)
+            print("Wrote counts to `"+fullPath+"`")
+    print("Finish processing "+gff3File)
+print("Script complete")
