f361b4bdf74db9f009d67be3c5f8a82dc75f812d
Philip Kahn <tigerhawkvok@gmail.com>
2017-12-13 16:00:00 -0800

    sample test

--- do-htseq-counts.py
+++ do-htseq-counts.py
@@ -15,6 +15,14 @@ import os
 dirName = "CountingResults"
 if not os.path.exists(dirName):
     os.mkdir(dirName)
+else:
+    import glob
+    i = 0
+    for oldRead in glob.glob(dirName+"/*counts.txt"):
+        os.unlink(oldRead)
+        i += 1
+    print("Removed "+str(i)+" old reads")
+
 
 # Args given:
 # -m union
@@ -25,17 +33,25 @@ if not os.path.exists(dirName):
 # -f bam
 
 fileSets = [
-    []
+    # Format:
+    # ["bamfile", "gff3", "label"]
+    ["AT-A-10_S4_L001_BowtieOut.sorted.q2.bam", "M-LYR-IN-11-CONTROLandDROUGHT.gff3", "CON1"]
 ]
 
 
 for i, filePair in enumerate(fileSets):
     print("Counting file "+str(i+1)+" of "+str(len(fileSets)))
+    ############################################
     # Default arguments and argument order interpreted from here:
     # https://github.com/simon-anders/htseq/blob/41ac2e51f64a1fb38129ceabf0f06a3e0e37825e/python3/HTSeq/scripts/count.py#L345-L455
+    #
+    # Sample:
+    #
+    # htseq-count -m union -s no --nonunique all -i gene_id -t exon -f bam AT-A-10_S4_L001_BowtieOut.sorted.q2.bam M-LYR-IN-11-CONTROLandDROUGHT.gff3 > M-LYR-IN-11-CONandDROUGHT-CON1-counts.txt
+    ############################################
     outputString = count.count_reads_in_features(
-        filePair[0],
-        filePair[1],
+        filePair[0], # BAM file
+        filePair[1], # GFF3 file
         "bam",
         "name",
         30000000,
@@ -51,7 +67,7 @@ for i, filePair in enumerate(fileSets):
         10,
         "")
     gff3 = filePair[1][:-5]
-    outFile = gff3 + "-counts.txt"
+    outFile = gff3 + "-" + filePair[2] + "-counts.txt"
     fullPath = dirName+"/"+outFile
     with open(fullPath, "w") as fileWriter:
         fileWriter.write(outputString)
